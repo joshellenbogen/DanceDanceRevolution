693811d12827dbde94da464c4d5be673266d1313
joshua_ellenbogen <joshua_ellenbogen@student.wayland.k12.ma.us>
2019-06-06 09:06:33 -0400

    All - on the same page now.

---
+++ src/BlackArrow.java
@@ -0,0 +1,8 @@
+public class BlackArrow extends Sprite {
+
+    public BlackArrow(int x, int y, int direction) {
+        super(x, y, direction);
+        setPic("BlackArrow.png", NORTH);
+        setSpeed(0);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
  
-//TODO: PUT YOUR NAME HERE
+//TODO: Josh Ellenbogen, Alex Kashian
  
 public class Main extends JPanel {
  
@@ -11,9 +11,21 @@ public class Main extends JPanel {
     public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
     private Timer timer;
     private boolean[] keys;
- 
+    private Sprite arrow1, arrow2, arrow3, arrow4;
+    private ArrayList<Sprite> arrows;
+
     public Main() {
-        keys = new boolean[512]; //should be enough to hold any key code.
+        keys = new boolean[512];
+
+        arrows = new ArrayList<Sprite>();
+
+        arrow1 = new BlackArrow(20, 20, Sprite.WEST);
+        arrow2 = new BlackArrow(220, 20, Sprite.SOUTH);
+        arrow3 = new BlackArrow(440, 20, Sprite.NORTH);
+        arrow4 = new BlackArrow(660, 20, Sprite.EAST);
+
+
+
 
         timer = new Timer(40, new ActionListener() {
             @Override
@@ -44,7 +56,10 @@ public class Main extends JPanel {
 
         }
 
-
+        arrow1.draw(g2);
+        arrow2.draw(g2);
+        arrow3.draw(g2);
+        arrow4.draw(g2);
 
     }
 

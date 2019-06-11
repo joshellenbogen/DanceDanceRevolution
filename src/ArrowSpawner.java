45a4bd84dbc46517ef4f8fe9ecfaa307130e3489
joshua_ellenbogen <joshua_ellenbogen@student.wayland.k12.ma.us>
2019-06-11 09:51:25 -0400

    get u up to date :) <3 ella

---
+++ src/ArrowSpawner.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ArrowSpawner extends Sprite {
+
+    private int arrowSpeed;
+    private ArrayList<BlueArrows> arrows;
+
+    public ArrowSpawner(int x, int y, int dir){
+        super(x, y, dir);
+        this.arrowSpeed = arrowSpeed;
+        arrows = new ArrayList<>();
+    }
+
+    public void update(Graphics2D g2){
+        for(BlueArrows arrow : arrows){
+            arrow.draw(g2);
+        }
+    }
+
+    public void spawn(){
+        arrows.add(new BlueArrows(getLoc().x, getLoc().y, getDir()));
+
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
@@ -17,40 +18,49 @@ public class Main extends JPanel {
     private Timer timer;
     private boolean[] keys;
     private Sprite arrow1, arrow2, arrow3, arrow4;
-    private ArrayList<Sprite> arrows;
+    private ArrayList<ArrowSpawner> arrowSpawners;
     private javafx.scene.media.MediaPlayer mediaPlayer;
+    private boolean a1, a2, a3, a4;
 
     public Main() {
         keys = new boolean[512];
 
-        arrows = new ArrayList<Sprite>();
+        arrowSpawners = new ArrayList();
+
+        arrowSpawners.add(new ArrowSpawner(20, 20, ArrowSpawner.WEST));
+        arrowSpawners.add(new ArrowSpawner(220, 20, ArrowSpawner.SOUTH));
+        arrowSpawners.add(new ArrowSpawner(440, 20, ArrowSpawner.NORTH));
+        arrowSpawners.add(new ArrowSpawner(660, 20, ArrowSpawner.EAST));
 
         arrow1 = new BlackArrow(20, 20, Sprite.WEST);
         arrow2 = new BlackArrow(220, 20, Sprite.SOUTH);
         arrow3 = new BlackArrow(440, 20, Sprite.NORTH);
         arrow4 = new BlackArrow(660, 20, Sprite.EAST);
 
-        arrows.add(new BlueArrow1(436, 700, Sprite.NORTH));
-        arrows.add(new BlueArrow2(216, 700, Sprite.NORTH));
-        arrows.add(new BlueArrow3(0, 700, Sprite.NORTH));
-        arrows.add(new BlueArrow4(670, 700, Sprite.NORTH));
-
         timer = new Timer(40, new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                for (Sprite s : arrows) {
-                    s.update();
-                }
                 repaint();
             }
         });
         JFXPanel fxPanel = new JFXPanel();
+        String aLot = "./res/ALot.mp3";
         String love = "./res/ILoveIt.mp3";
-        Media song = new Media(new File(love).toURI().toString());
+        Media song = new Media(new File(aLot).toURI().toString());
         mediaPlayer = new MediaPlayer(song);
         mediaPlayer.play();
 
         timer.start();
+
+        Timer spawnArrows = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                arrowSpawners.get(new Random().nextInt(4)).spawn();
+            }
+        });
+
+        spawnArrows.start();
 
         setKeyListener();
 
@@ -71,16 +81,16 @@ public class Main extends JPanel {
             }
 
         }
-
         arrow1.draw(g2);
         arrow2.draw(g2);
         arrow3.draw(g2);
         arrow4.draw(g2);
 
-        for (Sprite spr : arrows) {
-            spr.draw(g2);
+        for(ArrowSpawner arrowSpawner : arrowSpawners){
+            arrowSpawner.draw(g2);
         }
-    }
+
+        }
 
         public void setKeyListener () {
             addKeyListener(new KeyListener() {
@@ -91,6 +101,41 @@ public class Main extends JPanel {
                 @Override
                 public void keyPressed(KeyEvent keyEvent) {
                     keys[keyEvent.getKeyCode()] = true;
+
+                    int count = 0;
+                    if (keyEvent.VK_LEFT == keyEvent.getKeyCode()){
+                        a1 = true;
+                    }
+                    if (keyEvent.VK_DOWN == keyEvent.getKeyCode()){
+                        a2 = true;
+                    }
+                    if (keyEvent.VK_UP == keyEvent.getKeyCode()){
+                        a3 = true;
+                    }
+                    if (keyEvent.VK_RIGHT == keyEvent.getKeyCode()){
+                        a4 = true;
+                    }
+
+                    for (Sprite spr : arrowSpawners) {
+                        if (a1 == true && spr.getLoc().y >0 && spr.getLoc().y < 70)
+                            count++;
+                        if (a2 == true && spr.getLoc().y >0 && spr.getLoc().y < 70)
+                            count++;
+                        if (a3 == true && spr.getLoc().y >0 && spr.getLoc().y < 70)
+                            if(spr.getLoc().x < 460 && spr.getLoc().x > 420)
+                                count++;
+                        if (a4 == true && spr.getLoc().y >0 && spr.getLoc().y < 70)
+                            count++;
+
+                        System.out.println(count);
+
+                    }
+
+
+                    a1 = false;
+                    a2 = false;
+                    a3 = false;
+                    a4 = false;
                 }
 
                 //when a key is released, its boolean is switched to false.
